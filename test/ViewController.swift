7eddb1f054db00a42edcc63c984f4f2c1b88d461
vinokuroviv <vinokurov_iv@mail.ru>
2018-12-16 09:28:57 +0300

    Удаление лишних комментариев

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -1,10 +1,4 @@
-//
-//  ViewController.swift
-//  test
-//
-//  Created by Винокуров И.В. on 16/12/2018.
-//  Copyright © 2018 Винокуров И.В. All rights reserved.
-//
+
 
 import UIKit
 
@@ -12,9 +6,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
 
